913f217602c7d2a6487e42578db9baeac4dd1f36
tomas-doudera <tomas-doudera@users.noreply.github.com>
2023-07-26 10:55:53 +0000

    Fix styling

--- src/FilamentSimpleColorPickerServiceProvider.php
+++ src/FilamentSimpleColorPickerServiceProvider.php
@@ -16,16 +16,16 @@ class FilamentSimpleColorPickerServiceProvider extends PluginServiceProvider
     protected array $widgets = [];
 
     protected array $styles = [
-//        'plugin-filament-simple-color-picker' => __DIR__.'/../resources/dist/filament-simple-color-picker.css',
+        //        'plugin-filament-simple-color-picker' => __DIR__.'/../resources/dist/filament-simple-color-picker.css',
     ];
 
     protected array $scripts = [
-//        'plugin-filament-simple-color-picker' => __DIR__.'/../resources/dist/filament-simple-color-picker.js',
+        //        'plugin-filament-simple-color-picker' => __DIR__.'/../resources/dist/filament-simple-color-picker.js',
     ];
 
-     protected array $beforeCoreScripts = [
-//         'plugin-filament-simple-color-picker' => __DIR__ . '/../resources/dist/filament-simple-color-picker.js',
-     ];
+    protected array $beforeCoreScripts = [
+        //         'plugin-filament-simple-color-picker' => __DIR__ . '/../resources/dist/filament-simple-color-picker.js',
+    ];
 
     public function configurePackage(Package $package): void
     {
